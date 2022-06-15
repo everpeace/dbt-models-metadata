7ea343fc2fc1e36a7566540a16921d4813000d6d
Shingo Omura <everpeace@gmail.com>
2022-06-15 20:47:12 +0900

    reduce adapter dependent part in metadata_columns macro

---
+++ macros/heleprs/convert_result.sql
@@ -0,0 +1,85 @@
+{% macro convert_timing_to_dict(timing) %}
+    {#
+        Transform timing in result to dict so that it can work with "|tojson" filter 
+            TODO: how to make "|tojson" work??
+    #}
+    {%- set timing_dict = [] -%}
+    {%- for tinfo in timing -%}
+        {%- do timing_dict.append({
+            "name": tinfo.name,
+            "started_at": modules.pytz.utc.localize(tinfo.started_at).isoformat(),
+            "completed_at": modules.pytz.utc.localize(tinfo.completed_at).isoformat(),
+        })-%}
+    {%- endfor -%}
+    {{return(timing_dict)}}
+{% endmacro %}
+
+{% macro convert_node_to_dict(node) %}
+    {#
+        Transform node in result to dict so that it can work with "|tojson" filter 
+            TODO: how to make "|tojson" work??
+    #}
+    {{return({
+        "raw_sql": node.raw_sql,
+        "compiled": node.compiled,
+        "database": node.database,
+        "schema": node.schema,
+        "fqn": node.fqn,
+        "unique_id": node.unique_id,
+        "package_name": node.package_name,
+        "root_path": node.root_path,
+        "path": node.path,
+        "original_file_path": node.original_file_path,
+        "name": node.name,
+        "resource_type": node.resource_type,
+        "alias": node.alias,
+        "checksum": {
+            "name": node.checksum.name,
+            "checksum": node.checksum.checksum,
+        },
+        "config": {
+            "enabled": node.config.enabled,
+            "alias": node.config.alias,
+            "schema": node.config.schema,
+            "database": node.config.database,
+            "tags": node.config.tags,
+            "meta": node.config.meta,
+            "materialized": node.config.materialized,
+            "persist_docs": node.config.persist_docs,
+            "post_hook": node.config.post_hook,
+            "pre_hook": node.config.pre_hook,
+            "quoting": node.config.quoting,
+            "column_types": node.config.column_types,
+            "full_refresh": node.config.full_refresh,
+            "unique_key": node.config.unique_key,
+            "on_schema_change": node.config.on_schema_change,
+            "grants": node.config.grants,
+        },
+        "tags": node.tags,
+        "refs": node.refs,
+        "sources": node.sources,
+        "depends_on": {
+            "macros": node.depends_on.macros,
+            "nodes": node.depends_on.nodes,
+            "nodes": node.depends_on.nodes,
+        },
+        "nodes": node.nodes if node.nodes else none,
+        "description": node.description,
+        "columns": node.columns,
+        "meta": node.meta,
+        "docs": {
+            "show": node.docs.show,
+        },
+        "patch_path": node.patch_path,
+        "compiled_path": node.compiled_path,
+        "build_path": node.build_path,
+        "deferred": node.deferred,
+        "unrendered_config": node.unrendered_config,
+        "created_at": node.created_at,
+        "config_call_dict": node.config_call_dict,
+        "compiled_sql": node.compiled_sql,
+        "extra_ctes_injected": node.extra_ctes_injected,
+        "extra_ctes": node.extra_ctes,
+        "relation_name": node.relation_name,
+    })}}
+{% endmacro %}
---
+++ macros/metadata_columns.sql
@@ -0,0 +1,98 @@
+{% macro metadata_columns(cfg, result=none, now=none) %}
+    {% set timing = [] %}
+    {% set node = {} %}
+
+    {%- if result is not none -%}
+        {% set timing = models_metadata.convert_timing_to_dict(result.timing) %}
+        {% set node = models_metadata.convert_node_to_dict(result.node) %}
+    {% endif %}
+
+    {# 
+        Column definition of models_metadata_columns
+        with values if result was passed
+    #}
+    {%- set column_types_mapping = adapter.dispatch('column_types_mapping', 'models_metadata')() -%}
+    {%- set column_list = [
+        {
+            "column": api.Column('unique_id', column_types_mapping['text']),
+            "value": result.node.unique_id if result is not none,
+        },
+        {
+            "column": api.Column('database', column_types_mapping['text']),
+            "value": result.node.database if result is not none,
+        },
+        {
+            "column": api.Column('schema', column_types_mapping['text']),
+            "value": result.node.schema if result is not none,
+        },
+        {
+            "column": api.Column('"table"', column_types_mapping['text']),
+            "column_name": 'table',
+            "value": result.node.name if result is not none,
+        },
+        {
+            "column": api.Column('description', column_types_mapping['text']),
+            "value": result.node.description if result is not none,
+        },
+
+        {
+            "column": api.Column('dbt_version', column_types_mapping['text']),
+            "value": dbt_version,
+        },
+        {
+            "column": api.Column('invocation_id', column_types_mapping['text']),
+            "value": invocation_id,
+        },
+        {
+            "column": api.Column('node', column_types_mapping['json']),
+            "value": node|tojson,
+        },
+        {
+            "column": api.Column('status', column_types_mapping['text']),
+            "value": result.status if result is not none,
+        },
+        {
+            "column": api.Column('thread_id', column_types_mapping['text']),
+            "value": result.thread_id if result is not none,
+        },
+        {
+            "column": api.Column('execution_time', column_types_mapping['float']),
+            "value": result.execution_time|tojson if result is not none,
+        },
+        {
+            "column": api.Column('timing', column_types_mapping['json']),
+            "value": timing|tojson if result is not none,
+        },
+        {
+            "column": api.Column('adapter_response', column_types_mapping['json']),
+            "value": result.adapter_response|tojson if result is not none,
+        },
+        {
+            "column": api.Column('message', column_types_mapping['text']),
+            "value": result.message if result is not none,
+        },
+        {
+            "column": api.Column('updated_at', column_types_mapping['timestamp']),
+            "value": now.isoformat() if now is not none,
+        },
+    ]-%}
+
+    {# 
+        transform list to dict
+            to make column existence check easier in later process
+    #}
+    {%- set columns = {} -%}
+    {%- for item in  column_list -%}
+        {%- set key = item["column_name"] if "column_name" in item else item["column"].name -%}
+        {%- do columns.update({
+            key: item
+        }) -%}
+    {%- endfor -%}
+
+    {# append user-defined additional_columns #}
+    {%- set _ = columns.update(
+        models_metadata.config__get_additional_columns(cfg),
+    )-%}
+
+    {{ return(columns) }}
+{% endmacro %}
