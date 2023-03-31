459fdb76837a04e0b11e8bea1c69df4551c8ee79
Eric Hagman <eric@instawork.com>
2023-03-31 15:35:36 -0600

    fix: increase 'text' size to 16x the original

--- macros/plugin/redshift/column_types_mapping.sql
+++ macros/plugin/redshift/column_types_mapping.sql
@@ -1,6 +1,6 @@
 {% macro redshift__column_types_mapping() %}
     {{return({
-        'text': 'character varying(1024)',
+        'text': 'character varying(16384)',
         'json': 'character varying(65535)',
         'float': 'double precision',
         'timestamp': 'timestamp with time zone'
